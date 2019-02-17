6ed5f0a0bd80a9e67b4a3c6652df143c7a262173
TakuyaTomioka <taku.netbusiness@gmail.com>
2019-02-17 23:42:20 +0900

    ADD delete_form on Postted articles

--- myproject/resources/views/posts/show.blade.php
+++ myproject/resources/views/posts/show.blade.php
@@ -10,5 +10,9 @@
     <small>Written on {{$post->created_at}}</small>
 
     <hr>
-<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=> 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
