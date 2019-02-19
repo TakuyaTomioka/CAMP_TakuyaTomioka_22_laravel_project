2e7420248a71c5c9bad89e033c3d8e9f0ed4facc
TakuyaTomioka <taku.netbusiness@gmail.com>
2019-02-20 07:54:14 +0900

    display user's own post on their login page

--- myproject/resources/views/home.blade.php
+++ myproject/resources/views/home.blade.php
@@ -15,6 +15,30 @@
                     @endif
                     <p class="text-center"><a href="/posts/create" class="btn btn-primary mb-5 mt-5">Create Post</a></p>
                     <h3 class="text-center">Your Blog Posts</h3>
+
+                    @if(count($posts) > 0)
+                        <table class="table table-striped mt-5">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{$post->title}}</td>
+                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a></td>
+                                    <td>
+                                        {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=> 'POST', 'class' => 'float-right'])!!}
+                                            {{Form::hidden('_method', 'DELETE')}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                        {!!Form::close()!!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You have no posts</p>
+                    @endif
                 </div>
             </div>
         </div>
--- myproject/resources/views/posts/show.blade.php
+++ myproject/resources/views/posts/show.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default post-space">Go Back</a>
+    <a href="/posts" class="btn btn-secondary post-space">Go Back</a>
     <h1 class="post-space">{{$post->title}}</h1>
     <div>
         {!!$post->body!!}
     </div>
     <hr>
-    <small>Written on {{$post->created_at}}</small>
+    <small>Written on {{$post->created_at}} by {{$post->user['name']}}</small>
 
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a>
     {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=> 'POST', 'class' => 'float-right'])!!}
         {{Form::hidden('_method', 'DELETE')}}
         {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
